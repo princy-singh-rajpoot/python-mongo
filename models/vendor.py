37fe0f36af4287f7dc189a1b6492a381e104981f
princy-singh-rajpoot <princyvandanasingh@gmail.com>
2024-05-30 02:09:35 +0530

    add docstring to model.py

--- models/vendor.py
+++ models/vendor.py
@@ -1,7 +1,20 @@
 from pydantic import BaseModel
 
-
 class Vendor(BaseModel):
+    """
+    Represents a vendor with detailed information regarding their performance and contact details.
+
+    Attributes:
+        name (str): The name of the vendor.
+        contact_details (str): The contact details of the vendor, such as phone number.
+        address (str): The physical address of the vendor.
+        vendor_code (str): A code assigned to the vendor for identification purposes.
+        on_time_delivery_rate (float): The percentage of deliveries made by the vendor on time.
+        quality_rating_avg (float): The average quality rating given to the vendor.
+        average_response_time (float): The average time (in hours) the vendor takes to respond to inquiries or issues.
+        fulfillment_rate (float): The percentage of orders fulfilled completely and accurately by the vendor.
+    """
+    
     name: str 
     contact_details: str
     address: str
@@ -10,4 +23,3 @@ class Vendor(BaseModel):
     quality_rating_avg: float 
     average_response_time: float 
     fulfillment_rate: float 
-
